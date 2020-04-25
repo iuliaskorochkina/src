96547689815d0f6b65b2340e427a9e52d2ad1ac3
YuliaSkorochkina <iulia.skorochkina@gmail.com>
2020-04-25 09:54:11 +0300

    Создала класс с конструкторами

--- StudentSkorochkina/Program.cs
+++ StudentSkorochkina/Program.cs
@@ -4,8 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace StudentSkorochkina
 {
+    public class Student 
+    {
+        private string group;
+        private string surname;
+        private string name;
+
+        public string Group
+        {
+            get { return this.group; }
+            set { this.group = value; }
+        }
+        public string Surname
+        {
+            get { return this.surname; }
+            set { this.surname = value; }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
